54382e1e90814b557281f4f899b3796007dc24f5
Afired <conor.huismann@gmail.com>
2022-03-14 15:57:02 +0100

    added prefab object with custom drawer in utils

---
+++ Assets/src/internal/Afired.Utils/Editor/PrefabPropertyDrawer.cs
@@ -0,0 +1,21 @@
+﻿using Afired.Utils.Prefabs;
+using UnityEditor;
+using UnityEngine;
+
+namespace Afired.Utils.Editor {
+    
+    [CustomPropertyDrawer(typeof(Prefab))]
+    public class PrefabPropertyDrawer : PropertyDrawer {
+        
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+            EditorGUI.BeginProperty(position, GUIContent.none, property);
+            SerializedProperty prefab = property.FindPropertyRelative("_prefab");
+            label.text += " (Prefab)";
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+            prefab.objectReferenceValue = EditorGUI.ObjectField(position, prefab.objectReferenceValue, typeof(GameObject), false);
+            EditorGUI.EndProperty();
+        }
+        
+    }
+    
+}
---
+++ Assets/src/internal/Afired.Utils/Prefabs/Prefab.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+
+namespace Afired.Utils.Prefabs {
+    
+    [Serializable]
+    public class Prefab {
+        
+        [SerializeField] private GameObject _prefab;
+        
+        public GameObject Instantiate() {
+            if(_prefab == null)
+                throw new Exception($"Prefab has not been assigned!");
+            GameObject instance = GameObject.Instantiate(_prefab);
+            return instance;
+        }
+        
+        public GameObject Instantiate(Vector3 position, Quaternion rotation) {
+            if(_prefab == null)
+                throw new Exception($"Prefab has not been assigned!");
+            GameObject instance = GameObject.Instantiate(_prefab, position, rotation);
+            return instance;
+        }
+        
+        public GameObject Instantiate(Transform parent) {
+            if(_prefab == null)
+                throw new Exception($"Prefab has not been assigned!");
+            GameObject instance = GameObject.Instantiate(_prefab, parent);
+            return instance;
+        }
+        
+        public GameObject Instantiate(Transform parent, bool worldPositionStays) {
+            if(_prefab == null)
+                throw new Exception($"Prefab has not been assigned!");
+            GameObject instance = GameObject.Instantiate(_prefab, parent, worldPositionStays);
+            return instance;
+        }
+        
+        public GameObject Instantiate(Vector3 position, Quaternion rotation, Transform parent) {
+            if(_prefab == null)
+                throw new Exception($"Prefab has not been assigned!");
+            GameObject instance = GameObject.Instantiate(_prefab, position, rotation, parent);
+            return instance;
+        }
+        
+    }
+    
+}
