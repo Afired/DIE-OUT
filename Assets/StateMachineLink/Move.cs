1bd2c889d59fde5f5420ff8761b0a3ea7fe312ad
Afired <conor.huismann@gmail.com>
2022-03-16 01:30:04 +0100

    experimental monobehaviour link

---
+++ Assets/StateMachineLink/Move.cs
@@ -0,0 +1,10 @@
+﻿using Afired.Utils.StatemachineExtensions;
+using UnityEngine;
+
+public class Move : StateMachineMonoBehaviour {
+    
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        Debug.Log("Move");
+    }
+    
+}
---
+++ Assets/src/internal/Afired.Utils/StatemachineExtensions/StateMachineMonoBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Afired.Utils.StatemachineExtensions {
+    
+    public abstract class StateMachineMonoBehaviour : MonoBehaviour {
+        
+        public virtual void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) { }
+        public virtual void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) { }
+        public virtual void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) { }
+        public virtual void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) { }
+        public virtual void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) { }
+        
+    }
+    
+}
