e5f223c66acee858f07f9c352912b23bce5a89c5
Afired <conor.huismann@gmail.com>
2022-03-30 08:04:46 +0200

    added horizontal movement to jump

--- Assets/StateMachineTest/JumpState.cs
+++ Assets/StateMachineTest/JumpState.cs
@@ -6,15 +6,22 @@ namespace StateMachineTest {
     
     [RequireComponent(typeof(Movable))]
     public class JumpState : State {
-        
+
+        [SerializeField] private float _speed = 10f;
         private Movable _movable;
+        private Vector2 _horizontal;
         
         private void Awake() {
             _movable = GetComponent<Movable>();
         }
         
         protected override void OnStateEnter() {
+            _horizontal = Animator.GetVector2("input.horizontal");
             _movable.AddVelocity(new Vector3(0, 10, 0));
+        }
+        
+        protected override void OnStateUpdate() {
+            _movable.Move(new Vector3(_horizontal.x, 0, _horizontal.y) * Time.deltaTime * _speed);
         }
         
     }
